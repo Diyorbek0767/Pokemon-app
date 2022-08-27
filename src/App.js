57236c35f389df456e9de7dfe7a3a0eae49e1bfa
Diyorbek Ozodjonov <diyorbekozodjonov4@gmail.com>
2022-08-27 16:40:38 +0500

    my pokemon app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React, { useState } from 'react'; 
+import axios from 'axios'; 
+import './App.css'; 
+ 
+const App = () => { 
+ const [pokemonName, setpokemonName] = useState(""); 
+ const [pokemon, setPokemon] = useState({ 
+  name: "", 
+  species: "", 
+  img: "", 
+  attack: "", 
+  defense: "", 
+  type: "" 
+ }) 
+ 
+ const fetchPokemon = () => { 
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`) 
+  .then((res) => { 
+   setPokemon({ 
+    name: pokemonName, 
+    species: res.data.species.name, 
+    img: res.data.sprites.front_default, 
+    attack: res.data.stats[1].base_stat, 
+    defense: res.data.stats[2].base_stat, 
+    type: res.data.types[0].type.name 
+   }); 
+   console.log(res) 
+  }) 
+  .catch(() => { 
+   console.error("Error!") 
+  }) 
+ } 
+ 
+ return ( 
+  <> 
+  
+   <div className="searchArea">
+    <input  
+     className="search" 
+     type="text"
+     placeholder='Name' 
+     onChange={(event) => { 
+      setpokemonName(event.target.value); 
+     }} 
+    /> 
+   </div>
+     <button className='searchBtn'  onClick={fetchPokemon}>Search</button> 
+ 
+   <div className="resultCard"> 
+    <div className="card"> 
+     <h1>{pokemon.name}</h1> 
+     <img src={pokemon.img} /> 
+     <p>Species: {pokemon.species}</p> 
+     <h3>Attack: {pokemon.attack}</h3> 
+     <h4>Protection: {pokemon.defense}</h4> 
+     <p>Type: {pokemon.type}</p> 
+    </div> 
+   </div> 
+  </> 
+ ); 
+}; 
+ 
 export default App;
